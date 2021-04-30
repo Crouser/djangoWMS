31605d9b6599d1ab95d0c61ffe3851bab46aa172
Crouser <talhaansari78670@gmail.com>
2021-05-01 00:03:17 +0530

    add 3 more parameters

--- data/models.py
+++ data/models.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 class Data(models.Model):
     temperature = models.DecimalField(decimal_places = 2,max_digits = 100)
     pressure = models.DecimalField(decimal_places = 2,max_digits = 100)
+    precipitation = models.DecimalField(decimal_places = 2,max_digits = 100,default= 0.00)
+    windDirection = models.CharField(max_length = 200,default="")
+    windSpeed = models.DecimalField(decimal_places = 2,max_digits = 100,default= 0.00)
     dateTime = models.DateTimeField(default=timezone.now)
 
   
